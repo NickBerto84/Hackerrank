6a5992acf4e6c914f209cbe67555cfbc7fac6409
NickBerto84 <31477809+NickBerto84@users.noreply.github.com>
2020-04-16 16:31:00 +0200

    Add files via upload
    Multi-level-inheritance C++ solution

---
+++ Multi-level-inheritance.cpp
@@ -0,0 +1,41 @@
+/* Multi-level-inheritance.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// https://www.hackerrank.com/challenges/multi-level-inheritance-cpp
+*/
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class Triangle {
+public:
+    void triangle() {
+        cout << "I am a triangle\n";
+    }
+};
+
+class Isosceles : public Triangle {
+public:
+    void isosceles() {
+        cout << "I am an isosceles triangle\n";
+    }
+};
+
+class Equilateral : public Isosceles {
+public:
+    void equilateral() {
+        cout << "I am an equilateral triangle\n";
+    }
+};
+
+
+int main() {
+    Equilateral eqr;
+    eqr.equilateral();
+    eqr.isosceles();
+    eqr.triangle();
+    return 0;
+}
