beadc02a677b93939bea45488d61c5c213545647
NickBerto84 <31477809+NickBerto84@users.noreply.github.com>
2020-04-24 12:37:57 +0200

    Create InputAndOutput.cpp
    InputAndOutput c++ solution

---
+++ InputAndOutput.cpp
@@ -0,0 +1,26 @@
+// InputAndOutput.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// https://www.hackerrank.com/challenges/cpp-input-and-output/problem
+//
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    //long double a, b, c, sum = 0;
+    int a, b, c, sum = 0;
+
+    cin >> a >> b >> c;
+
+    sum = a + b + c;
+
+    cout << sum;
+
+    return 0;
+}
